e9779fecf4714cf7ba9d78b34fb0c1cfe7781376
beastarman74 <44511355+beastarman74@users.noreply.github.com>
2024-12-09 16:09:43 +0000

    Update entrypoint.sh
    Amended to use config.ini file to support multiple backup folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cat << EOF > /var/spool/cron/crontabs/root
-${ROTATE_CRON} /usr/local/bin/rotate-backups ${ROTATE_OPTIONS} ${ROTATE_ADDITIONAL_OPTIONS} /path1 /path2 /path3 /path4 /path5 /path6 /path7 /path8 /path9 /path10 
+${ROTATE_CRON} /usr/local/bin/rotate-backups ${ROTATE_OPTIONS} --config /etc/rotate-backups.d/config.ini  
 EOF
 
 exec "$@"
